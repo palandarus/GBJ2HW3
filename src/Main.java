fa0a93f297ba8cea522328f743688429c31b8e0d
Denis Proshchenko <klarkken@yandex.ru>
2020-01-24 10:41:52 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Main {
+    private static ArrayList<String> list = new ArrayList<>();
+    private static String text = "У лукоморья дуб зелёный;\n" +
+            "Златая цепь на дубе том:\n" +
+            "И днём и ночью кот учёный\n" +
+            "Всё ходит по цепи кругом;\n" +
+            "Идёт направо — песнь заводит,\n" +
+            "Налево — сказку говорит.\n" +
+            "Там чудеса: там леший бродит,\n";
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
